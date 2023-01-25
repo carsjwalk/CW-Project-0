c639606f61b8c39b871964febe566fff17672cc2
Carson <walkercj11@vcu.edu>
2023-01-25 13:00:37 -0500

    Separate graphs by automatic and manual cars

--- mtcars plot.R
+++ mtcars plot.R
@@ -1,7 +1,22 @@
 library(ggplot2)
 library(dplyr)
-mtcars %>% 
+library(ggrepel)
+library(patchwork)
+manual <- mtcars %>% 
   filter(between(disp, 100, 1000)) %>% 
-  ggplot(aes(mpg, hp)) +
-  geom_point(aes(color = am))
+  group_by(am) %>% 
+  arrange(desc(am)) %>% 
+  ungroup() %>% 
+  slice(1:8)
+automatic <- mtcars %>% 
+  filter(between(disp, 100, 1000)) %>% 
+  group_by(am) %>% 
+  arrange(desc(am)) %>% 
+  ungroup() %>% 
+  slice(9:27)
+p1 <- ggplot(manual, aes(mpg, hp)) +
+  geom_point(aes(color = cyl))
 
+p2 <- ggplot(automatic, aes(mpg, hp)) +
+  geom_point(aes(color = cyl))
+(p1 | p2)
